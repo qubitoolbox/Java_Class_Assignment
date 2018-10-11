4d604b02499405464f46247c0b6cc3906c7209ff
qubitAI <osmanmorales@ymail.com>
2018-10-11 03:05:47 -0400

    Update BasicFile.java

--- src/BasicFile.java
+++ src/BasicFile.java
@@ -4,14 +4,16 @@ import javax.swing.*;
 import java.io.*;
 
 
-public class BasicFile {
+public class BasicFile 
+{
     
     //reserves a var of type FILE
     File f;
     
     
     
-    public BasicFile(){
+    public BasicFile()
+    {
         // object will point to root of directory
         JFileChooser choose = new JFileChooser(".");
         int status = choose.showOpenDialog(null);
@@ -28,7 +30,7 @@ public class BasicFile {
                     
                     display(f.getName(), "File has been chosen",JOptionPane.INFORMATION_MESSAGE);
                 
-        }
+         }
         catch(FileNotFoundException e){
             display("File not found....",e.toString(),JOptionPane.WARNING_MESSAGE);
         
@@ -37,63 +39,74 @@ public class BasicFile {
         
             display("Approve option was not selected", e.toString(), JOptionPane.ERROR_MESSAGE);
         
-        }}
+        }
+    }
         //displays to user
-        void display(String msg, String s, int t){
+        void display(String msg, String s, int t)
+        {
         
             JOptionPane.showMessageDialog(null,msg,s,t);
     
     
     }
         //returns the path of a file
-        String getPath(){
+        String getPath()
+        {
         
             return f.getAbsolutePath();
         
         }
         //returns the size of a file
-        long getFileSize(){
+        long getFileSize()
+        {
         
             return f.length();
         
         }
         //return the folder when file resides
-        String getFileParent(){
+        String getFileParent()
+        {
         
             return f.getParent();
         
         }
         //
-        long length(){
+        long length()
+        {
         
             return f.length();
         }
         //returns if chosen is a file
-        boolean isFile(){
+        boolean isFile()
+        {
         
             return isFile();
         
         }
         //permissions of file
-        String canRead(){
+        String canRead()
+        {
         
             return (f.canRead())?"This file can be opened for reading":"Cannot read this file";
         
         }
         //returns if directory is true
-        String directoryOrFile(){
+        String directoryOrFile()
+        {
         
             return(f.isDirectory())?"This is a directory and not an ordinary file":"This is a file and not a directory";
         
         }
         //returns if the file es existant or not
-        String exists(){
+        String exists()
+        {
             return (f.exists())?"The physical file exists":"The physical file does not exists";
         }
         
         
         /*
-        void printWriter() throws IOException{
+        void printWriter() throws IOException
+        {
         
            // int option = GetData.getInt(s)
            PrintWriter pw = new PrintWriter(f);
@@ -105,7 +118,8 @@ public class BasicFile {
         
         }*/
         //buffer method calculation starts
-        void unbufferedTime() throws IOException{
+        void unbufferedTime() throws IOException
+        {
     
             FileInputStream fis = new FileInputStream(f);
             System.out.println(f.getName() + " - " + f.length() + " bytes");
@@ -113,13 +127,10 @@ public class BasicFile {
             long startTime = System.nanoTime();
         
             while(fis.read() != -1)
-            ;
         
             long endTime = System.nanoTime();
             System.out.println("Time    elapse   when   unbuffered" + (endTime-startTime)/1000000.0+ "msec" );
-    
-    
-    }
+        }
     
         void bufferedTime()throws IOException{
     
@@ -137,27 +148,25 @@ public class BasicFile {
     
     }
         //read a text file, line by line of character
-        void readLineNumberByLine(File f) throws IOException{
-        
+        void readLineNumberByLine(File f) throws IOException
+        {
             LineNumberReader lnr = null;
-            
             try {
-            
-            
                 lnr = new LineNumberReader(new FileReader(f));
                 String line = "";
-                
                 while((line = lnr.readLine()) != null)
                     JOptionPane.showMessageDialog(null, "Line" +  lnr.getLineNumber() + ":" + line);
-                
             }
             
-            finally{
-                try{
+            finally
+            {
+                try
+                {
                     lnr.close();
                 
                 }
-                catch(IOException e){
+                catch(IOException e)
+                {
                     e.printStackTrace();
                 
                 }
